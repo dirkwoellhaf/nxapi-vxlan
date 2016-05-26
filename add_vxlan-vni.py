e2bd77fe461552f5d24d40b5e3848c04f8862537
dirkwoellhaf <dirk.woellhaf@gmail.com>
2016-05-26 13:22:30 +0200

    add vn-segment

--- add_vxlan-vni.py
+++ add_vxlan-vni.py
@@ -1,6 +1,6 @@
 #/usr/bin/env oython
 
-import requests
+#import requests
 import json
 
 """
@@ -9,6 +9,10 @@ Modify these please
 url='http://10.1.18.13/ins'
 switchuser='admin'
 switchpassword='C15co123!'
+cmd_id_start = 2
+vland_id_start = 101
+vni_id_start = vland_id_start + 40000
+
 
 myheaders={'content-type':'application/json-rpc'}
 payload=[
@@ -29,7 +33,25 @@ payload=[
       "version": 1
     },
     "id": 2
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "name PY_100",
+      "version": 1
+    },
+    "id": 3
+  },
+  {
+    "jsonrpc": "2.0",
+    "method": "cli",
+    "params": {
+      "cmd": "vn-segment 100",
+      "version": 1
+    },
+    "id": 4
   }
 ]
-print payload
-#response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+#print payload
+response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
