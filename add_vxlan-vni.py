afa82d865a3a27df715e18b6aee73e194c93e109
dirkwoellhaf <dirk.woellhaf@gmail.com>
2016-05-26 13:31:23 +0200

    .

--- add_vxlan-vni.py
+++ add_vxlan-vni.py
@@ -11,47 +11,52 @@ switchuser='admin'
 switchpassword='C15co123!'
 cmd_id_start = 2
 vland_id_start = 101
-vni_id_start = vland_id_start + 40000
+vxlan_id_start = vland_id_start + 40000
+
+def addVlan (vlan_id, vxlan_id)
+
+    myheaders={'content-type':'application/json-rpc'}
+    payload=[
+      {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": "conf t",
+          "version": 1
+        },
+        "id": 1
+      },
+      {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": "vlan "+str(vlan_id),
+          "version": 1
+        },
+        "id": 2
+      },
+      {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": "name PY_"+str(vlan_id),
+          "version": 1
+        },
+        "id": 3
+      },
+      {
+        "jsonrpc": "2.0",
+        "method": "cli",
+        "params": {
+          "cmd": "vn-segment "+str(vxlan_id),
+          "version": 1
+        },
+        "id": 4
+      }
+    ]
+    #print payload
+    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
 
 
-myheaders={'content-type':'application/json-rpc'}
-payload=[
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "conf t",
-      "version": 1
-    },
-    "id": 1
-  },
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "vlan 100",
-      "version": 1
-    },
-    "id": 2
-  },
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "name PY_100",
-      "version": 1
-    },
-    "id": 3
-  },
-  {
-    "jsonrpc": "2.0",
-    "method": "cli",
-    "params": {
-      "cmd": "vn-segment 40100",
-      "version": 1
-    },
-    "id": 4
-  }
-]
-#print payload
-response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+if __name__ == "__main__":
+    addVlan(vland_id_start,vxlan_id_start)
