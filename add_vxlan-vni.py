a378f95036f79af320678b3da85ec25de3f8247d
dirkwoellhaf <dirk.woellhaf@gmail.com>
2016-05-27 23:27:35 +0200

    clean-up

--- add_vxlan-vni.py
+++ add_vxlan-vni.py
@@ -16,13 +16,10 @@ needed = int(sys.argv[2])  #MAX: 3500
 nve_interface = 1
 mcast_base = "239.0.0.0"
 
-def getMcastAddress(mcast_base,counter,needed):
+def getMcastAddress(mcast_base):
     mcast = mcast_base.split(".")
-    mcast[3] = counter
+    #mcast[3] = counter
 
-    #test = needed/255
-    #print int((counter/test)/1.5)
-    #print random.randint(1,)
     mcast[3] = random.randint(1,512)
     if mcast[3] >= 257:
         mcast[2] = 1
@@ -36,51 +33,6 @@ def getMcastAddress(mcast_base,counter,needed):
 def addSegment (vlan_id,vxlan_id,mcast_address,nve_interface):
 
     print "Creating VLAN-ID:"+str(vlan_id)+", VXLAN-ID:"+str(vxlan_id)+", MCAST-GROUP:"+str(mcast_address)
-
-    myheaders={'content-type':'application/json-rpc'}
-    payload=[
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "conf t","version": 1 }, "id": 1
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "vlan "+str(vlan_id), "version": 1 }, "id": 2
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "name PY_"+str(vlan_id), "version": 1 }, "id": 3
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "vn-segment "+str(vxlan_id), "version": 1 }, "id": 4
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "interface nve "+str(nve_interface), "version": 1 }, "id": 5
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "member vni "+str(vxlan_id), "version": 1 }, "id": 6
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "mcast-group "+str(mcast_address), "version": 1 }, "id": 7
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "router bgp 65000", "version": 1 }, "id": 8
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "evpn", "version": 1 }, "id": 9
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "vni "+str(vxlan_id)+" l2", "version": 1 }, "id": 10
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "rd auto", "version": 1 }, "id": 11
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "route-target import auto", "version": 1 }, "id": 12
-      },
-      {
-        "jsonrpc": "2.0", "method": "cli", "params": { "cmd": "route-target export auto", "version": 1 }, "id": 13
-      }
-
-
-    ]
 
     myheaders={'content-type':'application/json'}
     payload={
@@ -107,7 +59,7 @@ if __name__ == "__main__":
     vxlan_id = vxlan_id_start
 
     while counter <= needed:
-        response = addSegment(vland_id,vxlan_id,getMcastAddress(mcast_base,counter,needed),nve_interface)
+        response = addSegment(vland_id,vxlan_id,getMcastAddress(mcast_base),nve_interface)
 
         if "error" in response :
             print "ERROR: "+str(response)
