4102d11766b2418ea96a97746dbc40587f7d46db
dirkwoellhaf <dirk.woellhaf@gmail.com>
2016-05-27 23:43:28 +0200

    .

--- add_vxlan-vni.py
+++ add_vxlan-vni.py
@@ -5,20 +5,19 @@ import json
 import random
 import sys
 
-url='http://'+sys.argv[1]+'/ins'
+
 switchuser='admin'
 switchpassword='C15co123!'
 cmd_id_start = 2
 vland_id_start = 100
 vxlan_id_start = vland_id_start + 40000
 counter = 1
-needed = int(sys.argv[2])  #MAX: 3500
+needed = int(sys.argv[1])  #MAX: 3500
 nve_interface = 1
 mcast_base = "239.0.0.0"
 
 def getMcastAddress(mcast_base):
     mcast = mcast_base.split(".")
-    #mcast[3] = counter
 
     mcast[3] = random.randint(1,512)
     if mcast[3] >= 257:
@@ -30,7 +29,7 @@ def getMcastAddress(mcast_base):
     return address
 
 
-def addSegment (vlan_id,vxlan_id,mcast_address,nve_interface):
+def addSegment (vlan_id,vxlan_id,mcast_address,nve_interface,switches):
 
     print "Creating VLAN-ID:"+str(vlan_id)+", VXLAN-ID:"+str(vxlan_id)+", MCAST-GROUP:"+str(mcast_address)
 
@@ -41,13 +40,16 @@ def addSegment (vlan_id,vxlan_id,mcast_address,nve_interface):
         "type": "cli_conf",
         "chunk": "0",
         "sid": "1",
-        "input": "conf t ;vlan "+str(vlan_id)+" ;name PY_"+str(vlan_id)+" ;vn-segment "+str(vxlan_id)+" ;interface nve "+str(nve_interface)+" ;member vni "+str(vxlan_id)+" ;mcast-group "+str(mcast_address)+" ;router bgp 65000 ;evpn ;vni "+str(vxlan_id)+" l2 ;rd auto ;route-target import auto ;route-target export auto",
+        "input": "vlan "+str(vlan_id)+" ;name PY_"+str(vlan_id)+" ;vn-segment "+str(vxlan_id)+" ;interface nve "+str(nve_interface)+" ;member vni "+str(vxlan_id)+" ;mcast-group "+str(mcast_address)+" ;router bgp 65000 ;evpn ;vni "+str(vxlan_id)+" l2 ;rd auto ;route-target import auto ;route-target export auto",
         "output_format": "json"
       }
     }
 
     #print payload
-    response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+    switches = switches.split(",")
+    for switch in switches:
+        url='http://'+switch+'/ins'
+        response = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
 
 # Enable for debugging
     #print response
@@ -58,8 +60,9 @@ if __name__ == "__main__":
     vland_id = vland_id_start
     vxlan_id = vxlan_id_start
 
+
     while counter <= needed:
-        response = addSegment(vland_id,vxlan_id,getMcastAddress(mcast_base),nve_interface)
+        response = addSegment(vland_id,vxlan_id,getMcastAddress(mcast_base),nve_interface,sys.argv[2])
 
         if "error" in response :
             print "ERROR: "+str(response)
